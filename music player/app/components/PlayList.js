e1a62893e8a35f588cbaf7f813b1a11de1fbbe31
SP-Upreti <surendraupreti441@gmail.com>
2024-06-23 19:02:25 +0545

    update content

--- music player/app/components/PlayList.js
+++ music player/app/components/PlayList.js
@@ -1,19 +1,29 @@
-import Card from "./PlayCard";
-
-export default function PlayList() {
+export default function PlayList({ onSongSelect, data }) {
     return (
         <div className="flex flex-col gap-5  pe-5 h-[90vh] my-8 p-12 w-fit">
             <h2 className="font-semibold text-2xl">Play Lists</h2>
             <div className="flex flex-col gap-5 overflow-y-scroll pe-5  my-2  w-fit">
-                <Card />
-                <Card />
-                <Card />
-                <Card />
-                <Card />
-                <Card />
-                <Card />
-                <Card />
-                <Card />
+
+                {
+                    data.map(
+                        (song, key) => {
+                            return (
+                                <div className="" key={key} onClick={() => { onSongSelect(song) }}>
+                                    <div className="flex items-center gap-16  border border-slate-800 p-3 px-5 rounded-lg w-[22rem] cursor-pointer hover:shadow-xl card">
+                                        <div className="">
+                                            <h2 className="leading-4">{song.name}</h2>
+                                            <p className="font-semibold">{song.singer}</p>
+                                            <p className="font-semibold">{song.duration}</p>
+                                        </div>
+                                        <div className="h-16 w-16 overflow-hidden rounded-full flex justify-center items-center">
+                                            <img src={song.image} alt="" className="aspect-square" />
+                                        </div>
+                                    </div>
+                                </div>
+                            )
+                        }
+                    )
+                }
             </div>
         </div>
     )
--- music player/app/page.js
+++ music player/app/page.js
@@ -1,16 +1,25 @@
-
+"use client"
+import React, { useState } from "react";
 import PlayList from "./components/PlayList";
 import Player from "./components/Player";
+import data from "./data/data"
 
 export default function Home() {
-  
+  const [currentSong, setCurrentSong] = useState(data[0]);
+
+  // console.log(data[0])
+
+  const handleSongSelect = (song) => {
+    setCurrentSong(song);
+  };
+
   return (
-    <main className=" min-h-screen  flex root text-white ">
+    <main className="min-h-screen flex root text-white">
       <div className="w-[25%]">
-      <PlayList />
+        <PlayList onSongSelect={handleSongSelect} data={data} />
       </div>
       <div className="w-[75%]">
-      <Player />
+        <Player song={currentSong} />
       </div>
     </main>
   );
